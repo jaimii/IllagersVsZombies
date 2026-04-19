152eee38a3e5a1d31585e9ffe61e46c316f9a4c0
JustJaimii <jancarlodalacat@gmail.com>
2026-04-19 21:44:54 +0800

    Fixes everything, I hope.

--- src/main/java/project/kompass/illagerVZombies/MobAIListener.java
+++ src/main/java/project/kompass/illagerVZombies/MobAIListener.java
@@ -8,10 +8,13 @@ import net.minecraft.world.entity.monster.Giant;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.monster.Witch;
 import net.minecraft.world.entity.raid.Raider;
-import org.bukkit.craftbukkit.v1_21_R1.entity.CraftGiant;
-import org.bukkit.craftbukkit.v1_21_R1.entity.CraftRaider;
-import org.bukkit.craftbukkit.v1_21_R1.entity.CraftZombie;
-import org.bukkit.craftbukkit.v1_21_R1.entity.CraftWitch;
+
+// FIXED: Removed .v1_21_R1. from these imports
+import org.bukkit.craftbukkit.entity.CraftGiant;
+import org.bukkit.craftbukkit.entity.CraftRaider;
+import org.bukkit.craftbukkit.entity.CraftZombie;
+import org.bukkit.craftbukkit.entity.CraftWitch;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -20,26 +23,23 @@ public class MobAIListener implements Listener {
 
     @EventHandler
     public void onMobSpawn(EntitySpawnEvent event) {
-        // 1. Handle ALL RAIDERS (Pillagers, Vindicators, etc.)
+        // 1. Handle ALL RAIDERS
         if (event.getEntity() instanceof org.bukkit.entity.Raider bRaider) {
             Raider nmsRaider = ((CraftRaider) bRaider).getHandle();
             nmsRaider.goalSelector.addGoal(1, new AvoidEntityGoal<>(nmsRaider, Giant.class, 16.0F, 1.0D, 1.2D));
             nmsRaider.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(nmsRaider, Zombie.class, true));
         }
 
-        // 2. Handle WITCHES (Added Logic)
+        // 2. Handle WITCHES
         else if (event.getEntity() instanceof org.bukkit.entity.Witch bWitch) {
             Witch nmsWitch = ((CraftWitch) bWitch).getHandle();
-            // Witches also flee from Giants
             nmsWitch.goalSelector.addGoal(1, new AvoidEntityGoal<>(nmsWitch, Giant.class, 16.0F, 1.0D, 1.2D));
-            // Witches hunt Zombies
             nmsWitch.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(nmsWitch, Zombie.class, true));
         }
 
         // 3. Handle ZOMBIES
         else if (event.getEntity() instanceof org.bukkit.entity.Zombie bZombie) {
             Zombie nmsZombie = ((CraftZombie) bZombie).getHandle();
-            // Zombies hunt both Raiders and Witches
             nmsZombie.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(nmsZombie, Raider.class, true));
             nmsZombie.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(nmsZombie, Witch.class, true));
         }
@@ -48,8 +48,6 @@ public class MobAIListener implements Listener {
         else if (event.getEntity() instanceof org.bukkit.entity.Giant bGiant) {
             Giant nmsGiant = ((CraftGiant) bGiant).getHandle();
             nmsGiant.goalSelector.addGoal(0, new MeleeAttackGoal(nmsGiant, 1.0D, true));
-
-            // Giants hunt both Raiders and Witches
             nmsGiant.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(nmsGiant, Raider.class, true));
             nmsGiant.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(nmsGiant, Witch.class, true));
 
